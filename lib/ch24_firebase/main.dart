c38f8927f801eae249cdecc676449db8316603c3
sooojChoi <cmleon88@naver.com>
2023-12-23 21:32:38 +0900

    firebase chapter is added.

---
+++ lib/ch24_firebase/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const FirebaseApp());
+}
+
+class FirebaseApp extends StatelessWidget {
+  const FirebaseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
